08d3751cea8b37dc41eb70d216f26b0fc079d17a
Bartixxx32 <mcbplay1@gmail.com>
2020-02-11 19:31:25 +0100

    Create cda_dl-docker.sh

---
+++ cda_dl-docker.sh
@@ -0,0 +1,7 @@
+file="cdafolder.txt"
+if [ -f $file ] ; then
+    rm $file
+fi
+lynx -dump -listonly --nonumbers $1 | grep video/ | sed '$!N; /^\(.*\)\n\1$/!P; D' >> $file
+youtube-dl -a $file -f best -o "Pobrane/%(title)s.%(ext)s"
+rm -r $file
