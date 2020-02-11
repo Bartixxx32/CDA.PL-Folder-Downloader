c117cd3266a7e5b3832e31659b4b765b24b8dc2a
Bartixxx32 <mcbplay1@gmail.com>
2020-02-11 21:10:46 +0100

    miss

--- cda_dl-docker.sh
+++ cda_dl-docker.sh
@@ -3,5 +3,5 @@ if [ -f $file ] ; then
     rm $file
 fi
 lynx -dump -listonly --nonumbers $1 | grep video/ | sed '$!N; /^\(.*\)\n\1$/!P; D' >> $file
-youtube-dl -a $file -f best -o "Pobrane/%(title)s.%(ext)s" --external-downloader-args '-c -j 10 -x 3 -s 3 -k 1M'
+youtube-dl -a $file -f best -o "Pobrane/%(title)s.%(ext)s" --external-downloader aria2c --external-downloader-args '-c -j 10 -x 3 -s 3 -k 1M'
 rm -r $file
