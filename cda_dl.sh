53155d68e3357cec19f7743af2f36420400b5407
Bartixxx32 <mcbplay1@gmail.com>
2020-02-11 19:29:25 +0100

    Update cda_dl.sh

--- cda_dl.sh
+++ cda_dl.sh
@@ -3,5 +3,5 @@ if [ -f $file ] ; then
     rm $file
 fi
 lynx -dump -listonly --nonumbers $1 | grep video/ | sed '$!N; /^\(.*\)\n\1$/!P; D' >> $file
-youtube-dl -a $file -f best -o "./Pobrane/%(title)s.%(ext)s"
+youtube-dl -a $file -f best -o $2/"%(title)s.%(ext)s"
 rm -r $file
