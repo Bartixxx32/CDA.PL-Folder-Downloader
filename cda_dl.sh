a8e4b67eb233c1bdb6b0d3b23e218a78ebbd3d22
Bartixxx32 <mcbplay1@gmail.com>
2020-02-13 13:39:27 +0100

    multithread

--- cda_dl.sh
+++ cda_dl.sh
@@ -1,7 +1,4 @@
-file="cdafolder.txt"
-if [ -f $file ] ; then
-    rm $file
-fi
+file=$(mktemp temp.XXXXXXXXX)
 lynx -dump -listonly --nonumbers $1 | grep video/ | sed '$!N; /^\(.*\)\n\1$/!P; D' >> $file
 youtube-dl -a $file -f best -o $2/"%(title)s.%(ext)s" --external-downloader aria2c --external-downloader-args '-c -j 10 -x 3 -s 3 -k 1M'
 rm -r $file
