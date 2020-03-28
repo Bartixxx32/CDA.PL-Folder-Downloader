08ed1d1b286eccb1bf3e7f85070f0d7860408659
Bartixxx32 <mcbplay1@gmail.com>
2020-03-28 16:33:51 +0100

    Update cda_dl-docker.sh

--- cda_dl-docker.sh
+++ cda_dl-docker.sh
@@ -1,4 +1,5 @@
-file=$(mktemp temp.XXXXXXXXX)
-lynx -dump -listonly --nonumbers $1 | grep video/ | sed '$!N; /^\(.*\)\n\1$/!P; D' >> $file
-youtube-dl -a $file -f best -o "Pobrane/%(title)s.%(ext)s" --external-downloader aria2c --external-downloader-args '-c -j 10 -x 3 -s 3 -k 1M'
-rm -r $file
+echo CDA aktualnie zmienilo sposob pobierania z ich strony, skrypt na ten czas będzie niedostępny
+#file=$(mktemp temp.XXXXXXXXX)
+#lynx -dump -listonly --nonumbers $1 | grep video/ | sed '$!N; /^\(.*\)\n\1$/!P; D' >> $file
+#youtube-dl -a $file -f best -o "Pobrane/%(title)s.%(ext)s" --external-downloader aria2c --external-downloader-args '-c -j 10 -x 3 -s 3 -k 1M'
+#rm -r $file
