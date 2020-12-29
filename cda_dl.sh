a8f8d910c658c3518189b8865f37ed9356ee156f
Bartixxx <mcbplay1@gmail.com>
2020-12-29 23:16:45 +0100

    fix

--- cda_dl.sh
+++ cda_dl.sh
@@ -2,5 +2,4 @@ file=$(mktemp temp.XXXXXXXXX)
 lynx -dump -listonly --nonumbers $1 | grep video/ | sed '$!N; /^\(.*\)\n\1$/!P; D' >> $file
 xargs -n 1 -I{} sh bridge.sh {} <$file
 aria2c -i lista.txt -d Pobrane --auto-file-renaming=false -c -j 10 -x 3 -s 3 -k 1M
-mv *.mp4 Pobrane
 rm -r $file
