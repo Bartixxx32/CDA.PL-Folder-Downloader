4e4869be9e0d2f5565e874f1f73c531d857d90d0
Bartixxx <mcbplay1@gmail.com>
2021-03-13 02:35:54 +0100

    Revert "Update cda_dl-docker.sh"
    This reverts commit 7b0597f1064dd0489bbd2cef0837a2b3b5073ebb.

--- cda_dl-docker.sh
+++ cda_dl-docker.sh
@@ -1,4 +1,5 @@
 file=$(mktemp temp.XXXXXXXXX)
 lynx -dump -listonly --nonumbers $1 | grep video/ | sed '$!N; /^\(.*\)\n\1$/!P; D' >> $file
-youtube-dl --sleep-interval 30 -a $file -f best -o "Pobrane/%(title)s.%(ext)s" --retries infinite --no-check-certificate --external-downloader aria2c --external-downloader-args '-c -j 10 -x 10 -s 3 -k 1M'
+youtube-dl -a $file -f best -o "Pobrane/%(title)s.%(ext)s" --retries infinite --no-check-certificate --external-downloader aria2c --external-downloader-args '-c -j 10 -x 10 -s 3 -k 1M'
+sleep 15
 rm -r $file
