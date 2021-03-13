bdc80c47dfdb2eae3a6b691bd88f8d224e7c11d1
Bartixxx <mcbplay1@gmail.com>
2021-03-13 02:35:59 +0100

    Revert "-x 10"
    This reverts commit 2bfc0500114ed6e58678aaae095315940e86a40c.

--- cda_dl-docker.sh
+++ cda_dl-docker.sh
@@ -1,4 +1,4 @@
 file=$(mktemp temp.XXXXXXXXX)
 lynx -dump -listonly --nonumbers $1 | grep video/ | sed '$!N; /^\(.*\)\n\1$/!P; D' >> $file
-youtube-dl -a $file -f best -o "Pobrane/%(title)s.%(ext)s" --external-downloader aria2c --external-downloader-args '-c -j 10 -x 10 -s 3 -k 1M'
+youtube-dl -a $file -f best -o "Pobrane/%(title)s.%(ext)s" --external-downloader aria2c --external-downloader-args '-c -j 10 -x 3 -s 3 -k 1M'
 rm -r $file
