b17cbf56d07b26c178e048328ce75e653ec4fd47
Bartixxx32 <mcbplay1@gmail.com>
2023-06-28 19:17:26 +0000

    fix

--- cda_dl.sh
+++ cda_dl.sh
@@ -1,6 +1,5 @@
 file=$(mktemp temp.XXXXXXXXX)
 lynx -dump -listonly --nonumbers $1 | grep video/ | sed '$!N; /^\(.*\)\n\1$/!P; D' | sort | uniq >> $file
-xargs -n 1 -I{} sh bridge.sh {} <$file
-aria2c -i lista.txt -d Pobrane --auto-file-renaming=false -c -j 10 -x 3 -s 3 -k 1M
+yt-dlp -ciw -a $file -o "Pobrane/%(title)s.%(ext)s" --external-downloader aria2c --external-downloader-args '-c -j 10 -x 3 -s 3 -k 1M'
 rm -r lista.txt
 rm -r $file
